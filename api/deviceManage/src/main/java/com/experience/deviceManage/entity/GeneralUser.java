db86ef3eac84107ab6135bcc2730824339420b42
poshichao <1648907448@qq.com>
2018-12-25 09:51:35 +0800

    初始化后台普通用户接口

---
+++ api/deviceManage/src/main/java/com/experience/deviceManage/entity/GeneralUser.java
@@ -0,0 +1,40 @@
+package com.experience.deviceManage.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class GeneralUser {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;        // 姓名
+    private String email;       // 邮箱
+    private String password;    // 密码
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
