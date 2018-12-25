3120e08c2522841ce380c800e1614efd7aa19aa7
poshichao <1648907448@qq.com>
2018-12-25 08:53:23 +0800

    初始化后台

---
+++ api/deviceManage/src/main/java/com/experience/deviceManage/DeviceManageApplication.java
@@ -0,0 +1,14 @@
+package com.experience.deviceManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManageApplication.class, args);
+	}
+
+}
+
---
+++ api/deviceManage/src/test/java/com/experience/deviceManage/DeviceManageApplicationTests.java
@@ -0,0 +1,17 @@
+package com.experience.deviceManage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeviceManageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
