f5cf70c1ee33e2d90009bf1cd3c79ba79b9165d2
poshichao <1648907448@qq.com>
2018-12-25 11:07:43 +0800

    初始化前台

---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testApp
+ */
+angular.module('testApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
