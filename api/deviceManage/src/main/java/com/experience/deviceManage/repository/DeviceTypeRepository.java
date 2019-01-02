973b2fe678a38ecaa1b52564442e2ebb8dd3d4a9
poshichao <1648907448@qq.com>
2019-01-02 10:44:32 +0800

    按名称获取用户

--- api/deviceManage/src/main/java/com/experience/deviceManage/repository/DeviceTypeRepository.java
+++ api/deviceManage/src/main/java/com/experience/deviceManage/repository/DeviceTypeRepository.java
@@ -1,12 +1,13 @@
 package com.experience.deviceManage.repository;
 
 import com.experience.deviceManage.entity.DeviceType;
+import com.experience.deviceManage.entity.InlineDeviceType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource(path = "deviceType")
+@RepositoryRestResource(path = "deviceType", excerptProjection = InlineDeviceType.class)
 public interface DeviceTypeRepository extends CrudRepository<DeviceType, Long> {
     @RestResource
     DeviceType findByName(@Param("name") String name);
--- api/deviceManage/src/main/java/com/experience/deviceManage/service/UserServiceImpl.java
+++ api/deviceManage/src/main/java/com/experience/deviceManage/service/UserServiceImpl.java
@@ -2,17 +2,27 @@ package com.experience.deviceManage.service;
 
 import com.experience.deviceManage.entity.GeneralUser;
 import com.experience.deviceManage.entity.LaboratoryUser;
+import com.experience.deviceManage.repository.GeneralUserRepository;
+import com.experience.deviceManage.repository.LaboratoryUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private GeneralUserRepository generalUserRepository;
+    @Autowired
+    private LaboratoryUserRepository laboratoryUserRepository;
+
     @Override
     public void generalRegister(GeneralUser generalUser) {
-        // todo:普通用户注册
+        generalUserRepository.save(generalUser);
     }
 
     @Override
     public void laboratoryRegister(LaboratoryUser laboratoryUser) {
-        // todo:实验室用户注册
+        laboratoryUserRepository.save(laboratoryUser);
     }
+
+
 }
