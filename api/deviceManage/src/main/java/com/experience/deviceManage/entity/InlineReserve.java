f437bcd67f95d4d9bbd9c9339ab4fd4f3037b777
poshichao <1648907448@qq.com>
2019-01-03 08:45:24 +0800

    添加字段

---
+++ api/deviceManage/src/main/java/com/experience/deviceManage/entity/InlineReserve.java
@@ -0,0 +1,16 @@
+package com.experience.deviceManage.entity;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.Date;
+
+@Projection(name = "inlineReserve", types = Reserve.class)
+public interface InlineReserve {
+    Long getId();
+    Date getStartTime();
+    Date getEndTime();
+    String getPhone();
+    Byte getStatus();
+    Device getDevice();
+    String getComment();
+}
